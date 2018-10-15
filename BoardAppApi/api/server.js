f532078f229a82358ddcc46f534001ec3b0c23bb
Josh Kofoot <JoshK@spotcable.com>
2018-10-15 13:56:13 -0500

    Linked up routes controller and model

--- BoardAppApi/api/server.js
+++ BoardAppApi/api/server.js
@@ -1,6 +1,17 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/BoardGameModel.js');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/blah');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/BoardGameModel'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
