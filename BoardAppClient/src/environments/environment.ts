bb75589a62a0503500594d19df2ad5b2b28bd228
Josh Kofoot <JoshK@spotcable.com>
2018-10-18 13:30:04 -0500

    Added api urls for different enviorments

--- BoardAppClient/src/environments/environment.ts
+++ BoardAppClient/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ApiUrl: "Http://192.168.1.15:3000/"
 };
 
 /*
