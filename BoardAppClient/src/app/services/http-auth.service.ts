669e5cb6dc3e3449bf33dbe677a05589131b23d2
Josh Kofoot <jko330@gmail.com>
2020-07-03 22:12:41 +0100

    Fixed slack messages not making it through

--- BoardAppClient/src/app/services/http-auth.service.ts
+++ BoardAppClient/src/app/services/http-auth.service.ts
@@ -11,13 +11,15 @@ export class HttpAuthService implements HttpInterceptor {
   
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
-    let userToken = this.userService.userId;
-    if (userToken) {
-        request = request.clone({
-            setHeaders: { 
-                Authorization: `Bearer ${userToken}`
-            }
-        });
+    if(request.url.indexOf('slack') <= -1){
+      let userToken = this.userService.userId;
+      if (userToken) {
+          request = request.clone({
+              setHeaders: { 
+                  Authorization: `Bearer ${userToken}`
+              }
+          });
+      }
     }
 
     return next.handle(request);
